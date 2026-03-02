7ae4578f3ed16ca858ebddac06ae529f8f13c65e
Monito <phil.menard1668@gmail.com>
2026-03-02 15:51:02 +0100

    Initiate project
    Added a script to player so he can move and jump. Jump higher is jump button is held. Added a light as a child of player to learn more about it

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+	[SerializeField] private InputActionAsset inputActions;
+	[SerializeField] private float speed = 5.0f;
+	[SerializeField] private float jumpForce = 5.0f;
+	[SerializeField] private float jumpHoldDuration = .5f;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private Vector2 groundCheckSize = new Vector2(1, 0.1f);
+	private Rigidbody2D rb;
+	private InputAction moveAction;
+	private InputAction jumpAction;
+	private Vector2 direction;
+	private float jumpDuration;
+	private bool isJumping = false;
+	private bool isGrounded = true;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		moveAction = InputSystem.actions.FindAction("Move");
+		jumpAction = InputSystem.actions.FindAction("Jump");
+		jumpDuration = jumpHoldDuration;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		isGrounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0f, groundLayer);
+		direction = moveAction.ReadValue<Vector2>();
+		JumpInput();
+	}
+
+	void FixedUpdate()
+	{
+		rb.linearVelocity = new Vector2(direction.x * speed, rb.linearVelocityY);
+
+		if (isJumping)
+		{
+			rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+		}
+	}
+
+	void JumpInput()
+	{
+		if (jumpAction.WasPressedThisFrame() && isGrounded)
+			isJumping = true;
+
+		if (jumpAction.WasReleasedThisFrame() || jumpHoldDuration <= 0)
+		{
+			isJumping = false;
+			jumpHoldDuration = jumpDuration;
+		}
+
+		if (isJumping)
+			jumpHoldDuration -= Time.deltaTime;
+	}
+
+	void OnDrawGizmos()
+	{
+		if (groundCheck == null)
+			return;
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
+	}
+}
